44fa2f5992de814c0462874b7c7115dcb824a885
Mark Dale <msdaledad@gmail.com>
2021-12-31 17:36:40 -0500

    App appears to be fully functional...will continue to test

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,6 +1,16 @@
+const path = require('path');
 const router = require('express').Router();
-const noteRoutes = require('../apiRoutes/noteRoutes');
 
-router.use(noteRoutes);
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
+
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 
 // Use apiRoutes
 app.use('/api', apiRoutes);
-//app.use('/', htmlRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
